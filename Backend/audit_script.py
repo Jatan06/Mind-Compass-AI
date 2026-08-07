8ceaaa34764a8a7536ab2b736dedd830da066585
Jatan_06 <parikhjatan54@gmail.com>
2026-08-08 01:27:59 +0530

    chore: remove validation_probe.py script

--- Backend/audit_script.py
+++
@@ -1,6 +0,0 @@
-import json
-from activities.models import TherapyActivity
-
-activities = list(TherapyActivity.objects.values('id', 'title', 'category', 'difficulty', 'duration'))
-with open('filtered_activities.json', 'w', encoding='utf-8') as f:
-    json.dump(activities, f, indent=4)
